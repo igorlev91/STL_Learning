c317e3158e08748f3960725ef2f6b75e0a9c67c0
Igor <igorlevdansk91@gmail.com>
2022-05-12 22:35:09 +0300

    Add pair implementation

---
+++ Tests/PairTests.cpp
@@ -0,0 +1,10 @@
+#pragma once
+#include "../Std/Pair.h"
+using namespace CustomStd;
+
+TEST_CASE("Pair ALL", "[Pair]")
+{
+	Pair<int, double> p(1, 5.0);
+	Pair<int, double> p2(p);
+	REQUIRE(p == p2);
+}
